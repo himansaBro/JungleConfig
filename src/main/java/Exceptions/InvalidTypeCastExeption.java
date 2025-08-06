5ac34f903de4fcca5df9684be7fe1542bb029ef9
Himansa <90212151+himansaBro@users.noreply.github.com>
2025-08-06 17:24:07 +0530

    Delete src/main/java directory
    Deleted Old Version

--- src/main/java/Exceptions/InvalidTypeCastExeption.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class InvalidTypeCastExeption extends RuntimeException{
-    public InvalidTypeCastExeption(String message) {
-        super(message);
-    }
-}
--- src/main/java/core/dataTypes/JConfigConverter.java
+++
@@ -1,7 +0,0 @@
-package core.dataTypes;
-
-public interface JConfigConverter <T>{
-    public Class<T> getType();
-    public String Encode(Object data);
-    public Object Decode(String data);
-}
